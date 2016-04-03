8708407e2dfa986058233700579e9b95ce145926
Torniojaws <torniojaws@gmail.com>
2016-04-03 03:11:58 +0300

    Added modal windows to band members.

--- templates/partials/bio.php
+++ templates/partials/bio.php
@@ -1,6 +1,7 @@
 <div class="row">
     <div class="col-sm-3">
-        <a href="static/img/<?php echo strtolower($photo['name_id']) . "/"; echo $photo['full']; ?>">
+        <!-- The photo will open in a modal window -->
+        <a href="#photo-modal<?php echo $photo['id']; ?>" data-toggle="modal" data-target="#photo-modal<?php echo $photo['id']; ?>">
             <img src="static/img/<?php echo strtolower($photo['name_id']) . "/";
             echo $photo['thumbnail']; ?>" alt="<?php echo $photo['caption']; ?>" />
         </a>
@@ -21,3 +22,23 @@
     </div>
 </div>
 <hr />
+
+<!-- Modal contents -->
+<div class="modal fade" id="photo-modal<?php echo $photo['id']; ?>" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title"><?php echo $photo['name'].' - '.$photo['date_taken']; ?></h4>
+            </div>
+            <div class="modal-body text-center">
+                <img src="static/img/<?php echo strtolower($photo['name_id']) . "/";
+                echo $photo['thumbnail']; ?>" alt="<?php echo $photo['caption']; ?>" />
+                <p><?php echo $photo['caption']; ?></p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
