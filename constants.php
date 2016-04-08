41b40e7212eaa359883a13688a8ccadf60a1ddd4
Torniojaws <torniojaws@gmail.com>
2016-04-08 06:54:17 +0300

    Define testing

--- constants.php
+++ constants.php
@@ -3,10 +3,14 @@
     // Constants for the site
 
     # Use this if Vagrant is setup to redirect 5656 to port 5656 in guest
-    define('SERVER_URL', 'http://localhost:5656/');
+    #define('SERVER_URL', 'http://localhost:5656/');
 
     # Use this if Vagrant is setup to redirect 5656 to port 80 in guest
     #define('SERVER_URL', "http://localhost/");
+
+    # Testing if this is portable
+    $url = 'http://'.$_SERVER['SERVER_NAME'].'/';
+    define('SERVER_URL', $url);
 
     # This is the base url in production
     #define('SERVER_URL', 'http://www.vortechmusic.com');
