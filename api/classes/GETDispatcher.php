5ea4e8604cd11b995424c6545cf03c9f9b63c1ae
Torniojaws <torniojaws@gmail.com>
2016-04-13 09:27:41 +0300

    A little include tweak to API

--- api/classes/GETDispatcher.php
+++ api/classes/GETDispatcher.php
@@ -1,16 +1,5 @@
 <?php
 
-    require_once 'NewsAPI.php';
-    require_once 'ReleasesAPI.php';
-    require_once 'ShowsAPI.php';
-    require_once 'PhotosAPI.php';
-    require_once 'VisitorCountAPI.php';
-    require_once 'MembersAPI.php';
-    require_once 'VideosAPI.php';
-    require_once 'ShopAPI.php';
-    require_once 'GuestbookAPI.php';
-    require_once 'UsersAPI.php';
-
     class GETDispatcher
     {
         private $sql;
@@ -22,42 +11,52 @@
             $root = $request[1];
             switch ($root) {
                 case 'news':
+                    require_once 'NewsAPI.php';
                     $result = new NewsAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'releases':
+                    require_once 'ReleasesAPI.php';
                     $result = new ReleasesAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'shows':
+                    require_once 'ShowsAPI.php';
                     $result = new ShowsAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'photos':
+                    require_once 'PhotosAPI.php';
                     $result = new PhotosAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'members':
+                    require_once 'MembersAPI.php';
                     $result = new MembersAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'videos':
+                    require_once 'VideosAPI.php';
                     $result = new VideosAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'visitors':
+                    require_once 'VisitorCountAPI.php';
                     $result = new VisitorCountAPI($request);
                     $this->sql = $result->getResult();
                     break;
                 case 'shopitems':
+                    require_once 'ShopAPI.php';
                     $result = new ShopAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'guestbook':
+                    require_once 'GuestbookAPI.php';
                     $result = new GuestbookAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
                 case 'users':
+                    require_once 'UsersAPI.php';
                     $result = new UsersAPI($request, $filters);
                     $this->sql = $result->getResult();
                     break;
