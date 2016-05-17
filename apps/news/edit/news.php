d76b8f585120b8ccd83b744c079ae1e5808aa410
Torniojaws <torniojaws@gmail.com>
2016-05-17 14:14:31 +0300

    Converted Main, News, Photos and Shop edits to use a class

--- apps/news/edit/news.php
+++ apps/news/edit/news.php
@@ -1,53 +1,14 @@
 <?php
 
-    session_start();
-    require_once '../../../constants.php';
+    $root = str_replace('apps/news/edit', '', __DIR__);
+    require_once $root.'classes/EditValue.php';
 
-    /*
-        From news, we expect to edit the Title, Contents and Tags.
-        TODO: Tags are sent as html, so will need some other way - DB query?
-    */
-    if (isset($_SESSION) && $_SESSION['authorized'] == 1 && isset($_POST)) {
-        list($column, $id) = explode('-', $_POST['id']);
-        $new_value = $_POST['value'];
-
-        // PUT http://www.vortechmusic.com/api/v1/news/123
-        $api = SERVER_URL.'api/v1/news/'.$id;
-
-        // Generate the PUT request
-        $data = array(
-            'id' => $id,
-            'column' => $column,
-            'new_value' => $new_value,
-        );
-        $options = array(
-            'http' => array(
-                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-                'method' => 'PUT',
-                'content' => http_build_query($data),
-            ),
-        );
-        $context = stream_context_create($options);
-        $result = file_get_contents($api, false, $context);
-        if ($result === false) {
-            $response['status'] = 'error';
-            $response['message'] = 'Could not update to DB';
-        } else {
-            $response['status'] = 'success';
-            $response['message'] = 'Updated DB successfully';
-        }
-    } else {
-        if (isset($_SESSION['authorized']) == false) {
-            header('HTTP/1.1 401 Unauthorized');
-            exit;
-        } else {
-            $response['status'] = 'error';
-            $response['message'] = 'Missing required data';
-        }
-    }
+    $category = 'news';
+    $edit = new EditValue($_POST, $category, $root);
+    $response = $edit->commitEdit();
 
     if ($response['status'] == 'success') {
-        echo $new_value;
-    } else {
         echo $_POST['value'];
+    } else {
+        echo 'Failed to update! '.$response['message'].$_POST['value'];
     }
