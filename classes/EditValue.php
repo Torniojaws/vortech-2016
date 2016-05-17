8a6de4dfd185d6d0a91e6fe41f8768e1f89017b7
Torniojaws <torniojaws@gmail.com>
2016-05-17 14:29:25 +0300

    Small tweak to EditValues and converted some more things

--- classes/EditValue.php
+++ classes/EditValue.php
@@ -24,10 +24,16 @@
                 $this->column = $column;
                 $this->new_value = $postData['value'];
                 // Category is eg. "videos" or "releases"
+                // for clarity, we add the trailing slash here, but
+                // only if the url does not contain parameters
+                if (strpos($category, '?') === false) {
+                    // No parameters in url, so we'll add a trailing slash
+                    $category .= '/';
+                }
                 $this->category = $category;
 
                 // eg. PUT http://www.vortechmusic.com/api/v1/videos/123
-                $this->endpoint = SERVER_URL.'api/v1/'.$category.'/'.$id;
+                $this->endpoint = SERVER_URL.'api/v1/'.$category.$id;
                 $this->payload = $this->buildRequest();
             } else {
                 header('HTTP/1.1 401 Unauthorized');
